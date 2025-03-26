492be6f0773ebacf2bfb33248c6a8bd62e5bf43a
zirtoshka <367222@edu.itmo.ru>
2025-03-26 17:54:54 +0300

    init

---
+++ vk-zv/ms-vk-kkoshel/src/main/java/itma/msvkkkoshel/MsVkKkoshelApplication.java
@@ -0,0 +1,13 @@
+package itma.msvkkkoshel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVkKkoshelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVkKkoshelApplication.class, args);
+	}
+
+}
---
+++ vk-zv/ms-vk-kkoshel/src/test/java/itma/msvkkkoshel/MsVkKkoshelApplicationTests.java
@@ -0,0 +1,13 @@
+package itma.msvkkkoshel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVkKkoshelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
